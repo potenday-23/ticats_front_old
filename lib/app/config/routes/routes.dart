684a26fb67a0c6301b61ff3e1f12324d74d303d6
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-04 18:28:28 +0900

    feat: 앱 기초 설정
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

---
+++ lib/app/config/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:get/route_manager.dart';
+import 'package:tickets/app/config/routes/route_path.dart';
+import 'package:tickets/presentation/home/page/home_page.dart';
+
+class Routes {
+  static List<GetPage> routes = [
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:tickets/app/config/routes/route_path.dart';
+import 'package:tickets/app/config/routes/routes.dart';
+import 'package:tickets/presentation/home/controller/home_binding.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,15 +13,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       theme: ThemeData(
         fontFamily: 'SUIT',
+        useMaterial3: true,
+      ),
       ),
       home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
       ),
+      getPages: Routes.routes,
     );
   }
 }
