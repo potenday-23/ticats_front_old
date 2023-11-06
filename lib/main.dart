dd69a84170163f71232633221ba52d13e7d62b28
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-07 01:55:09 +0900

    fix: 로그인 관련 누락된 요소들 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/route_manager.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:tickets/app/config/app_binding.dart';
 import 'package:tickets/app/config/routes/route_path.dart';
 import 'package:tickets/app/config/routes/routes.dart';
 
-void main() {
+void main() async {
   runApp(const MainApp());
+
+  // Load .env file
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Kakao SDK
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']!);
 }
 
 class MainApp extends StatelessWidget {
