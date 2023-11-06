e5fc9522052b1b1ea75e5aa8115bbd0b328f3a16
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-07 00:10:19 +0900

    feat: 카카오 로그인 기능 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/app/config/app_binding.dart
+++ lib/app/config/app_binding.dart
@@ -1,6 +1,13 @@
 import 'package:get/get.dart';
+import 'package:tickets/app/service/auth_service.dart';
 
 class AppBinding extends Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    injectService();
+  }
+
+  void injectService() {
+    Get.put<AuthService>(AuthService());
+  }
 }
