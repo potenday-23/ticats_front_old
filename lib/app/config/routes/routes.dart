09081fcb07dc015a4e79ed77be1862628bbd021f
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-07 01:57:02 +0900

    feat: 로그인 화면 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/app/config/routes/routes.dart
+++ lib/app/config/routes/routes.dart
@@ -1,8 +1,14 @@
 import 'package:get/route_manager.dart';
 import 'package:tickets/app/config/routes/route_path.dart';
-import 'package:tickets/presentation/home/page/home_page.dart';
+import 'package:tickets/presentation/authentication/view/login_page.dart';
+import 'package:tickets/presentation/authentication/view/term_agree_page.dart';
 
 class Routes {
   static List<GetPage> routes = [
+    // Authentication - Login
+    GetPage(
+      name: RoutePath.login,
+      page: () => const LoginPage(),
+    ),
   ];
 }
