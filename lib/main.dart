dd8dea355de06faae69ee95df89671dca934725d
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-06 22:32:47 +0900

    feat: ScreenUtil 초기화 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/route_manager.dart';
+import 'package:tickets/app/config/app_binding.dart';
 import 'package:tickets/app/config/routes/route_path.dart';
 import 'package:tickets/app/config/routes/routes.dart';
-import 'package:tickets/presentation/home/controller/home_binding.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,18 +14,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-        fontFamily: 'SUIT',
-        useMaterial3: true,
-      ),
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-      getPages: Routes.routes,
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      useInheritedMediaQuery: true,
+      builder: (_, child) {
+        return GetMaterialApp(
+          theme: ThemeData(
+            fontFamily: 'SUIT',
+            useMaterial3: true,
+          ),
+          initialBinding: AppBinding(),
+          initialRoute: RoutePath.login,
+          getPages: Routes.routes,
+        );
+      },
     );
   }
 }
