58c0056dff681bbde74e8941befdb722541f7228
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-08 14:20:36 +0900

    feat: AppColor, AppTypeFace 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/app/config/app_typeface.dart
+++ lib/app/config/app_typeface.dart
@@ -1,1 +1,30 @@
-class AppTypeFace {}
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTypeFace {
+  // XXLarge
+  static TextStyle xxlargeBold = TextStyle(fontSize: 32.sp, fontWeight: FontWeight.w700);
+  static TextStyle xxlargeMedium = TextStyle(fontSize: 32.sp, fontWeight: FontWeight.w500);
+
+  // XLarge
+  static TextStyle xlargeBold = TextStyle(fontSize: 28.sp, fontWeight: FontWeight.w700);
+  static TextStyle xlargeSemiBold = TextStyle(fontSize: 28.sp, fontWeight: FontWeight.w600);
+
+  // Large
+  static TextStyle largeMedium = TextStyle(fontSize: 28.sp, fontWeight: FontWeight.w500);
+  static TextStyle largeSemiBold = TextStyle(fontSize: 26.sp, fontWeight: FontWeight.w600);
+
+  // Medium
+  static TextStyle mediumBold = TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w700);
+  static TextStyle mediumMedium = TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w500);
+
+  // Small
+  static TextStyle smallBold = TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w700);
+  static TextStyle smallSemiBold = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600);
+
+  // XSmall
+  static TextStyle xsmallSemiBold = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600);
+  static TextStyle xsmallMedium = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500);
+  static TextStyle xsmallBold = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w700);
+  static TextStyle xsmallSmall = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w400);
+}
