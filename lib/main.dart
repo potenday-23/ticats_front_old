161894c2b967c787800e885c48a772fbcd91390c
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-09 17:27:11 +0900

    feat: localization 추가
    - ko_KR
    
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/route_manager.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:tickets/app/config/app_binding.dart';
 import 'package:tickets/app/config/app_color.dart';
@@ -10,6 +13,10 @@ import 'package:tickets/app/config/routes/routes.dart';
 
 void main() async {
   runApp(const MainApp());
+
+  // Locale
+  await initializeDateFormatting();
+  Intl.defaultLocale = 'ko_KR';
 
   // Load .env file
   await dotenv.load(fileName: ".env");
@@ -29,6 +36,15 @@ class MainApp extends StatelessWidget {
       useInheritedMediaQuery: true,
       builder: (_, child) {
         return GetMaterialApp(
+          localizationsDelegates: const [
+            GlobalCupertinoLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('ko', 'KR'),
+          ],
+          locale: const Locale('ko', 'KR'),
           theme: ThemeData(
             fontFamily: 'SUIT',
             useMaterial3: true,
