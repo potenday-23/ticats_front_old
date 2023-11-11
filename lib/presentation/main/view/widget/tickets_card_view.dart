fdf1fe9103f2b2752a7cbc3d6c94856ec0e1f1ef
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-12 04:09:45 +0900

    fix: StackedCardCarousel pageController 충돌 해결
    - Another exception was thrown: The page property cannot be read when multiple PageViews are attached to the same PageController.
    
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/presentation/main/view/widget/tickets_card_view.dart
+++ lib/presentation/main/view/widget/tickets_card_view.dart
@@ -12,7 +12,9 @@ import '../../controller/ticket_controller.dart';
 import 'no_ticket_widget.dart';
 
 class TicketsCardView extends GetView<TicketController> {
-  const TicketsCardView({super.key, this.isAllTicket = false});
+  TicketsCardView({super.key, this.isAllTicket = false});
+
+  final PageController pageController = PageController();
 
   final bool isAllTicket;
 
@@ -27,76 +29,82 @@ class TicketsCardView extends GetView<TicketController> {
         } else if (ticketList.isEmpty) {
           return const NoTicketWidget();
         } else {
-          return StackedCardCarousel(type: StackedCardCarouselType.fadeOutStack, initialOffset: 10.w, spaceBetweenItems: 595.w, items: [
-            for (TicketModel ticket in ticketList) ...[
-              SizedBox(
-                width: 365.w,
-                height: 611.w,
-                child: Stack(
-                  children: [
-                    if (ticket.ticketType == '0') ...[
-                      if (ticket.layoutType == "0") Ticket1Small(ticket),
-                      if (ticket.layoutType == "1") Ticket1Medium(ticket),
-                      if (ticket.layoutType == "2") Ticket1Large(ticket),
-                    ] else if (ticket.ticketType == '1') ...[
-                      if (ticket.layoutType == "0") Ticket2Small(ticket),
-                      if (ticket.layoutType == "1") Ticket2Medium(ticket),
-                      if (ticket.layoutType == "2") Ticket2Large(ticket),
-                    ] else if (ticket.ticketType == '2') ...[
-                      if (ticket.layoutType == "0") Ticket3Small(ticket),
-                      if (ticket.layoutType == "1") Ticket3Medium(ticket),
-                      if (ticket.layoutType == "2") Ticket3Large(ticket),
-                    ],
-                    Positioned.fill(
-                      top: 60.h,
-                      right: 30.w,
-                      child: GestureDetector(
-                        onTap: () async {
-                          await TicketsDio().post('/likes/${ticket.id}');
-
-                          if (controller.likeTicketIdList.contains(ticket.id!)) {
-                            controller.likeTicketIdList.remove(ticket.id);
-                            controller.likeTicketList.remove(ticket);
-                          } else {
-                            controller.likeTicketIdList.add(ticket.id!);
-                            controller.likeTicketList.add(ticket);
-                            controller.likeTicketList.sort((a, b) => a.id!.compareTo(b.id!));
-                          }
-                        },
-                        child: Align(
-                          alignment: Alignment.topRight,
-                          child: controller.likeTicketIdList.contains(ticket.id)
-                              ? SvgPicture.asset(
-                                  'assets/icons/heart_fill.svg',
-                                  width: 24.w,
-                                )
-                              : SvgPicture.asset(
-                                  'assets/icons/heart.svg',
-                                  height: 24.w,
-                                ),
-                        ),
-                      ),
-                    ),
-                    Positioned.fill(
-                      bottom: 60.w,
-                      left: 40.w,
-                      child: GestureDetector(
+          return StackedCardCarousel(
+            type: StackedCardCarouselType.fadeOutStack,
+            pageController: pageController,
+            initialOffset: 10.w,
+            spaceBetweenItems: 595.w,
+            items: [
+              for (TicketModel ticket in ticketList) ...[
+                SizedBox(
+                  width: 365.w,
+                  height: 611.w,
+                  child: Stack(
+                    children: [
+                      if (ticket.ticketType == '0') ...[
+                        if (ticket.layoutType == "0") Ticket1Small(ticket),
+                        if (ticket.layoutType == "1") Ticket1Medium(ticket),
+                        if (ticket.layoutType == "2") Ticket1Large(ticket),
+                      ] else if (ticket.ticketType == '1') ...[
+                        if (ticket.layoutType == "0") Ticket2Small(ticket),
+                        if (ticket.layoutType == "1") Ticket2Medium(ticket),
+                        if (ticket.layoutType == "2") Ticket2Large(ticket),
+                      ] else if (ticket.ticketType == '2') ...[
+                        if (ticket.layoutType == "0") Ticket3Small(ticket),
+                        if (ticket.layoutType == "1") Ticket3Medium(ticket),
+                        if (ticket.layoutType == "2") Ticket3Large(ticket),
+                      ],
+                      Positioned.fill(
+                        top: 60.h,
+                        right: 30.w,
+                        child: GestureDetector(
                           onTap: () async {
-                            await showReportDialog(context, ticket);
+                            await TicketsDio().post('/likes/${ticket.id}');
+
+                            if (controller.likeTicketIdList.contains(ticket.id!)) {
+                              controller.likeTicketIdList.remove(ticket.id);
+                              controller.likeTicketList.remove(ticket);
+                            } else {
+                              controller.likeTicketIdList.add(ticket.id!);
+                              controller.likeTicketList.add(ticket);
+                              controller.likeTicketList.sort((a, b) => a.id!.compareTo(b.id!));
+                            }
                           },
                           child: Align(
-                              alignment: Alignment.bottomLeft,
-                              child: SvgPicture.asset(
-                                'assets/icons/report.svg',
-                                width: 20.w,
-                                height: 20.w,
-                              ))),
-                    ),
-                  ],
-                ),
-              )
+                            alignment: Alignment.topRight,
+                            child: controller.likeTicketIdList.contains(ticket.id)
+                                ? SvgPicture.asset(
+                                    'assets/icons/heart_fill.svg',
+                                    width: 24.w,
+                                  )
+                                : SvgPicture.asset(
+                                    'assets/icons/heart.svg',
+                                    height: 24.w,
+                                  ),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        bottom: 60.w,
+                        left: 40.w,
+                        child: GestureDetector(
+                            onTap: () async {
+                              await showReportDialog(context, ticket);
+                            },
+                            child: Align(
+                                alignment: Alignment.bottomLeft,
+                                child: SvgPicture.asset(
+                                  'assets/icons/report.svg',
+                                  width: 20.w,
+                                  height: 20.w,
+                                ))),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ],
-          ]);
+          );
         }
       },
     );
